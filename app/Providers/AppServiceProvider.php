58fa505a58667ea787d48b717826204a0ca893f3
zahyhabibi <zahyzer0000@gmail.com>
2025-08-21 05:46:57 -0700

    add environtment on app service provide with forscheme https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Kreait\Firebase\Factory;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -55,6 +56,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+        URL::forceScheme('https');
+        }
     }
 }
